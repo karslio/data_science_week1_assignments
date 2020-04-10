c9cd946deb744b30c5a8ac0bcd3123eea6de61f2
karslio <karslionl@gmail.com>
2020-04-10 23:09:55 +0200

    numpy_master.py finish

--- datascience-1.py
+++ datascience-1.py
@@ -282,18 +282,3 @@ print(np.median(a))
 print(np.min(a))
 # e.	en buyuk degeri bulunuz.
 print(np.max(a))
-
-# Master odev ;
-# Bir dongu icerisinde random olacak sekilde iki tane 20x20’luk matris uretin ve bu matrislerin farklarini alin.
-# Ve fark matrisinin diagonali,  -0.1 ile 0.1  arasinda olana kadar bu islemi tekrarlayin.
-# Istenilen matris bulundugunda program dursun ve  toplam kac dongunun kuruldugunu,
-# ne kadar sure icinde buldugunu ve  istenilen  matrisi  print ile birlikte ekrana yazdiriniz...
-# Tavsiye: 20 x 20 luk matrisin bulunmasi saatler surebilir. Bu yuzden algoritmanizin dogrulugunu test etmek icin
-# once 4x4, 5x5 gibi kucuk matrislerde deneyebilirsiniz. Ve matris sayisisini arttirarak en son 20x20 u deneyebilirsiniz.
-# Matris uretme islemini np.random.random((axb)) fonksiyonu ile yapabilirsiniz.(size kalmis) 1:30
-# Cuma aksamina kadar odevlerin hepsini gondermenizi rica ediyorum.
-# Ayrica, matris odevinin ekran goruntusunu yada ekran videosunu gruba atmanizi istiyorum.
-# Deneme matrislerini de gonderebilirsiniz. Hepinize kolay gelsin.
-#
-# Toplam 45 Soru
-#
--- numpy_master.py
+++ numpy_master.py
@@ -1,7 +1,21 @@
+# Master odev ;
+# Bir dongu icerisinde random olacak sekilde iki tane 20x20’luk matris uretin ve bu matrislerin farklarini alin.
+# Ve fark matrisinin diagonali,  -0.1 ile 0.1  arasinda olana kadar bu islemi tekrarlayin.
+# Istenilen matris bulundugunda program dursun ve  toplam kac dongunun kuruldugunu,
+# ne kadar sure icinde buldugunu ve  istenilen  matrisi  print ile birlikte ekrana yazdiriniz...
+# Tavsiye: 20 x 20 luk matrisin bulunmasi saatler surebilir. Bu yuzden algoritmanizin dogrulugunu test etmek icin
+# once 4x4, 5x5 gibi kucuk matrislerde deneyebilirsiniz. Ve matris sayisisini arttirarak en son 20x20 u deneyebilirsiniz.
+# Matris uretme islemini np.random.random((axb)) fonksiyonu ile yapabilirsiniz.(size kalmis) 1:30
+# Cuma aksamina kadar odevlerin hepsini gondermenizi rica ediyorum.
+# Ayrica, matris odevinin ekran goruntusunu yada ekran videosunu gruba atmanizi istiyorum.
+# Deneme matrislerini de gonderebilirsiniz. Hepinize kolay gelsin
+
+
 import numpy as np
 import time
 
 i = 0
+
 start = time.time()
 while True:
     a = np.random.rand(10, 10)
@@ -14,7 +28,7 @@ while True:
 
 end = time.time()
 time = end - start
-with open("print.txt", "w", encoding="utf-8") as file:
+with open("numpy_master.txt", "w", encoding="utf-8") as file:
     file.write("""
 import numpy as np
 import time
